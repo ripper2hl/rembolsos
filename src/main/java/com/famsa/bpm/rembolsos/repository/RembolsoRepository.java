bddfa2c728bd62e1124bdd6bc30f5b061a363f37
Jesús Perales <jesus.peralesmr@uanl.edu.mx>
2016-05-27 16:32:31 -0500

    opensfhit otro intento

--- src/main/java/com/famsa/bpm/rembolsos/repository/RembolsoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.famsa.bpm.rembolsos.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.famsa.bpm.rembolsos.model.Rembolso;
-
-public interface RembolsoRepository extends CrudRepository<Rembolso, Integer>{}
